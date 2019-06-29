383494eda348f7c40fb2102b42c31a70162fa1a9
trixobird <george.trichopoulos@gmail.com>
2019-06-29 09:42:25 +0300

    Initial Commit

---
+++ src/main/java/cy/meritkapital/tradevalidation/TradeValidationApplication.java
@@ -0,0 +1,13 @@
+package cy.meritkapital.tradevalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cy/meritkapital/tradevalidation/TradeValidationApplicationTests.java
@@ -0,0 +1,16 @@
+package cy.meritkapital.tradevalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TradeValidationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
